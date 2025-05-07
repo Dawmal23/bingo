d5eaabec376223de5eac1068101f7ccf06fa8cd3
fiflakrumuna <03052006uro@gmail.com>
2025-05-07 20:10:19 +0200

    reworked bingo table, css slightly modified

--- public/app.js
+++ public/app.js
@@ -1,12 +1,13 @@
 const socket = io();
+//socket - locking in server
 
 
-
-
+//main table clearing
 socket.on('clr_table', (val =>{
     document.getElementById('tabeleczka').innerHTML = ''
 }))
 
+//logging table setting
 socket.on('nicks', (nick =>{
     
     const table1 = document.getElementById('tabeleczka')
@@ -21,25 +22,40 @@ socket.on('nicks', (nick =>{
     table1.appendChild(tr)
 }))
 
+//user setting
 socket.on('currentUser', (currentUser) => {
     document.getElementById('napis').textContent = 'Gracz: '+currentUser;
     socket.emit('table_fill', currentUser)
 });
-socket.on('table_row', (cells) =>{
+
+//users bingo table creation
+socket.on('table_row', (full_tab_data) =>{
+    let cells = full_tab_data[0]
+    let completed = full_tab_data[1]
     const table1 = document.getElementById('tabeleczka')
     let tr = document.createElement('tr')
     
-    console.log(cells)
+    
     cells.forEach(row=>{
         let td = document.createElement('td')
-        td.textContent = row
+        let bingo_button = document.createElement('button')
+        bingo_button.textContent = row
+        
+        if(completed.shift()==1){
+            bingo_button.style.backgroundColor = rgb(133, 255, 188)
+            bingo_button.disabled = True
+        }
+        bingo_button.style.cssText = 'width: 100%; height: 100%; border: none; border-radius: 10%; background-color: #ffffff;'
+
+        td.appendChild(bingo_button)
         tr.appendChild(td)
     })
     console.log(tr)
     table1.appendChild(tr)
-}
-)
+})
 
+
+//W.I.P.
 socket.on('update-punkty', (punkty) => {
     document.getElementById('punkty').textContent = punkty;
 });
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//data manipulation variables
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -8,6 +9,8 @@ const server = http.createServer(app);
 const io = new Server(server);
 app.use(express.static('public'));
 
+
+//database connection
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -15,8 +18,10 @@ const db = mysql.createConnection({
     database: 'bingo'
 });
 
-
+//client-to-website connection scripts
 io.on('connection', (socket) => {
+
+    //login table creation
     console.log('Nowe połączenie:', socket.id);
     let val=''
     socket.emit('clr_table', val)
@@ -31,14 +36,12 @@ io.on('connection', (socket) => {
         } else {
             socket.emit('login-failed');
         }
+    })
 
-    let currentUser = null;
+    //bingo table creation and filling
     socket.on('tab_creation', (currentUser) => {
-        
-
         db.query('CALL `bingo`.`Create_Bingo_Card`(?)', [currentUser])
         db.query('SELECT COUNT(*) as counter from '+currentUser.toLowerCase(), (err, if_made)=>{
-            console.log(if_made[0].counter)
             if(if_made[0].counter<=0){
                 db.query('CALL `bingo`.`Fill_Bingo_Card`(?)', [currentUser])
             }
@@ -48,19 +51,34 @@ io.on('connection', (socket) => {
     socket.on('table_fill', (currentUser) =>{
         db.query("SELECT EventId FROM "+currentUser.toLowerCase(), (err, tab_files)=>{
             socket.emit('clr_table', val)
+            const completed_array = []
+            db.query("SELECT Completed from "+currentUser.toLowerCase(), (err, results)=>{
+                const Completed_list = results
+            
+            console.log(Completed_list)
+            Completed_list.forEach(completion=>{
+                completed_array.push(Completed_list[0].Completed)
+            })
             let cells = []
+            let completed = []
             if(tab_files.length > 0){
                 tab_files.forEach(EventId=>{
                     db.query("SELECT players.Name, events.Event FROM players JOIN events ON players.PlayerId = events.PerformerId WHERE events.EventId = ?", [EventId['EventId']], (err, events)=>{
                         if(cells.length<6){
                             event_string = ''+events[0].Name+' '+events[0].Event
                             cells.push(event_string)
-                    }else{
-                        socket.emit('table_row', cells)
-                        cells = []
+                            completed.push(completed_array.shift())
+                            console.log(completed)
+
+                        }else{
+                            full_tab_data = [cells, completed]
+                            socket.emit('table_row', full_tab_data)
+                            cells = []
+                            completed = []
                     }})
             })
             }
+        })
 
         
         });
@@ -70,7 +88,7 @@ io.on('connection', (socket) => {
 
    
     
-})});
+});
 
 
 server.listen(3000, () => {
