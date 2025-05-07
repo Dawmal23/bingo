336f783c826aee8f7ce8147f6bd2daede253475a
fiflakrumuna <03052006uro@gmail.com>
2025-05-07 22:31:57 +0200

    load of work done

--- public/app.js
+++ public/app.js
@@ -24,14 +24,19 @@ socket.on('nicks', (nick =>{
 
 //user setting
 socket.on('currentUser', (currentUser) => {
-    document.getElementById('napis').textContent = 'Gracz: '+currentUser;
+    let user_menu = document.getElementById('napis')
+    user_menu.textContent = 'Gracz: '+currentUser;
+    user_menu.dataset.tag = currentUser
+    document.getElementById('stylesheet').href = 'style-gra.css'
     socket.emit('table_fill', currentUser)
 });
 
-//users bingo table creation
+//users bingo table creation/edition
 socket.on('table_row', (full_tab_data) =>{
     let cells = full_tab_data[0]
     let completed = full_tab_data[1]
+    let events_ids = full_tab_data[2]
+
     const table1 = document.getElementById('tabeleczka')
     let tr = document.createElement('tr')
     
@@ -40,17 +45,31 @@ socket.on('table_row', (full_tab_data) =>{
         let td = document.createElement('td')
         let bingo_button = document.createElement('button')
         bingo_button.textContent = row
-        
-        if(completed.shift()==1){
-            bingo_button.style.backgroundColor = rgb(133, 255, 188)
-            bingo_button.disabled = True
+        bingo_button.setAttribute('id', events_ids.shift())
+        let block_id = completed.shift()
+        console.log(block_id)
+        bingo_button.style.cssText = 'width: 20vw; aspect-ratio: 1/1; border: none; border-radius: 100%; background-color: #ffffff;'
+        if(block_id==1){
+            bingo_button.disabled = 'True'
+            bingo_button.style.backgroundColor = 'rgb(133, 255, 188)'
+            
         }
-        bingo_button.style.cssText = 'width: 100%; height: 100%; border: none; border-radius: 10%; background-color: #ffffff;'
+        bingo_button.addEventListener('click', () => {
+            bingo_button.style.backgroundColor = 'rgb(133, 255, 188)'
+            bingo_button.disabled = 'True'
+            let completion_click = 1
+            let completion_id = bingo_button.id
+            let currentUser = document.getElementById('napis').dataset.tag
+            let update_button_data = []
+            update_button_data.push(completion_click)
+            update_button_data.push(completion_id)
+            update_button_data.push(currentUser)
+            socket.emit('bingo_button_click', update_button_data)
 
+        })
         td.appendChild(bingo_button)
         tr.appendChild(td)
     })
-    console.log(tr)
     table1.appendChild(tr)
 })
 
--- server.js
+++ server.js
@@ -49,33 +49,47 @@ io.on('connection', (socket) => {
         })
     });
     socket.on('table_fill', (currentUser) =>{
-        db.query("SELECT EventId FROM "+currentUser.toLowerCase(), (err, tab_files)=>{
+
+        db.query("SELECT EventId FROM "+currentUser.toLowerCase()+" ORDER BY id ASC", (err, tab_files)=>{
             socket.emit('clr_table', val)
             const completed_array = []
             db.query("SELECT Completed from "+currentUser.toLowerCase(), (err, results)=>{
                 const Completed_list = results
-            
-            console.log(Completed_list)
-            Completed_list.forEach(completion=>{
-                completed_array.push(Completed_list[0].Completed)
+                Completed_list.forEach(completion=>{
+                    completed_array.push(completion['Completed'])
             })
             let cells = []
             let completed = []
+            let events_ids = []
+
             if(tab_files.length > 0){
                 tab_files.forEach(EventId=>{
-                    db.query("SELECT players.Name, events.Event FROM players JOIN events ON players.PlayerId = events.PerformerId WHERE events.EventId = ?", [EventId['EventId']], (err, events)=>{
-                        if(cells.length<6){
+                    db.query("SELECT players.Name, events.Event, events.EventId FROM players JOIN events ON players.PlayerId = events.PerformerId WHERE events.EventId = ?", [EventId['EventId']], (err, events)=>{
+                        if(cells.length<5){
                             event_string = ''+events[0].Name+' '+events[0].Event
                             cells.push(event_string)
+                            let event_id = events[0].EventId
+                            events_ids.push(event_id)
                             completed.push(completed_array.shift())
-                            console.log(completed)
-
                         }else{
-                            full_tab_data = [cells, completed]
+                            full_tab_data = [cells, completed, events_ids]
                             socket.emit('table_row', full_tab_data)
                             cells = []
                             completed = []
-                    }})
+                            events_ids = []
+                            event_string = ''+events[0].Name+' '+events[0].Event
+                            cells.push(event_string)
+                            let event_id = events[0].EventId
+                            events_ids.push(event_id)
+                            completed.push(completed_array.shift())
+                            
+                        }
+                        if(completed_array.length==0){
+                            full_tab_data = [cells, completed, events_ids]
+                            socket.emit('table_row', full_tab_data)
+                        }
+                })
+
             })
             }
         })
@@ -83,6 +97,15 @@ io.on('connection', (socket) => {
         
         });
     });
+
+    //button update functions
+    socket.on('bingo_button_click', (update_button_data) =>{
+        let completion_click = update_button_data[0]
+        let completion_id = update_button_data[1]
+        let currentUser = update_button_data[2]
+        db.query("UPDATE "+currentUser.toLowerCase()+" SET Completed=1 WHERE EventId="+completion_id)
+        db.query("UPDATE players SET Score=(SELECT COUNT(*) FROM "+currentUser.toLowerCase()+" WHERE Completed=1) WHERE Name='"+currentUser+"'")
+    })
 
 
 
